e58fb5b71ab7b91d81058f7c2e6d59f521a76f27
zacharykeeton <zachary.keeton@gmail.com>
2015-08-13 11:48:49 -0500

    Added intro comments

--- WifiPasswordExtractor.bat
+++ WifiPasswordExtractor.bat
@@ -1,5 +1,9 @@
 @echo off
 
+rem Creates a file called 'ssidswkeys.txt' in the same directory with all the Wifi SSIDs that the computer
+rem remembers connecting to along with clear-text passwords if found.
+rem Zachary Keeton August 2015 - @ZacharyKeeton.  Inspired by the work of Aditya K Sood  (adi_ks [at] secniche.org)
+
 echo Working...
 
 del ssidswkeys.txt 2> nul
@@ -9,7 +13,6 @@ for /F "tokens=4*" %%i in ('netsh wlan show profiles *^| findstr /l :"SSID name"
 	echo ssid: %%i %%j >> ssidswkeys.txt 
 	netsh wlan show profile name=%%i %%j key=clear | findstr /l :"Key Content" >> ssidswkeys.txt
 	echo( >> ssidswkeys.txt
- 
 )
 
 echo Finished. See 'ssidskeys.txt'
